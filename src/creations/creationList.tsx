4d4b431a4760e079bcd0585124def5583665b6af
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2025-02-22 14:18:15 +0100

    router

--- src/creations/creationList.tsx
+++ src/creations/creationList.tsx
@@ -26,7 +26,7 @@ export default function CreationList({ creations, basePath }: Props) {
           lg={3} // Quatre cartes par ligne sur les très grands écrans
         >
           <Link
-            to={`/${basePath}/${creation.name}/${creation.id}`}
+            to={`/${basePath}/${creation.name}`}
             style={{ textDecoration: "none" }}
           >
             <CreationCard creation={creation} />
