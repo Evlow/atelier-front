3398318c90a05d8e1cfb9bb2a7d5da2a8d50054b
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2025-02-22 10:59:15 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
 import { Outlet } from 'react-router-dom';
 import './App.css'
-import HomePageCarrousel from './components/homePageCarrousel';
 
 function App() {
 
   return (
     <>
   <Outlet></Outlet>
-  <HomePageCarrousel></HomePageCarrousel>
     </>
   )
 }
